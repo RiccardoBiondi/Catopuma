204590496bd8385050d37c9641badf4ca0140f20
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2023-07-10 10:26:47 +0200

    [ENH] Add uploader
    Add a simple itk on the fly uploadeer class to load medical image formats. TODO: write its doc

---
+++ catopuma/uploader.py
@@ -0,0 +1,45 @@
+'''
+Module containing the implementation of the vaious Uploader to read and organize the input image
+and tergetes.
+'''
+
+import os
+from typing import Tuple
+import numpy as np
+import SimpleITK as sitk
+import tensorflow as tf
+
+from catopuma.core.abstraction import UploaderBase
+
+
+
+
+class SimpleITKUploader(UploaderBase):
+    '''
+    Class to upload and 
+    '''
+
+    def __init__(self, data_format: str = 'channels_last') -> None:
+
+        super().__init__()
+        if data_format in ['channels_first', 'channels_last']:
+            self.data_format = data_format
+        else:
+            raise ValueError(f'{data_format} is not allowed')
+
+    def __call__(self, *path: Tuple[str]) -> np.ndarray:
+        '''
+        Call method
+        '''
+
+        img = sitk.ReadImage(path[0]) # input path must be first
+        tar = sitk.ReadImage(path[1]) # target path must be second
+
+        # convert the images to array
+        img = sitk.GetArrayViewFromImage(img)[..., np.newaxis]
+        tar = sitk.GetArrayFromImage(tar)[..., np.newaxis]
+
+        if self.data_format == 'channels_first':
+            img = img.transpose(2, 0, 1)
+            tar = tar.transpose(2, 0, 1)
+        return img, tar
---
+++ test/test_uploader.py
@@ -0,0 +1,134 @@
+import pytest
+import hypothesis.strategies as st
+from hypothesis import given, settings
+
+
+from catopuma.uploader import SimpleITKUploader
+
+ALLOWED_DATA_FORMATS =   ('channels_first', 'channels_last')
+
+legitimate_chars = st.characters(whitelist_categories=('Lu', 'Ll'),
+                                 min_codepoint=65, max_codepoint=90)
+
+text_strategy = st.text(alphabet=legitimate_chars, min_size=1,
+                        max_size=15)
+
+def test_simple_itk_uploader_default_init():
+    '''
+    Test that all the dafault parametner are set correctly
+
+    Given:
+        - No argument is required
+    Then:
+        - init the SimpleITKUploader with default arguments
+    Assert:
+        all the class members are init to their default values
+    '''
+
+    loader = SimpleITKUploader()
+
+    assert loader.data_format == 'channels_last'
+
+
+@given(st.sampled_from(ALLOWED_DATA_FORMATS))
+def test_simple_itk_uploader_init(data_format: str):
+    '''
+    Check that SimpleITKUploader arguments are correctly instantiated
+
+    Given:
+        - valid data format
+    Then:
+        - init SimpleITKUploader
+    Assert:
+        - SimpleITKUploader data format is equal to the input one
+    '''
+    
+    loader = SimpleITKUploader(data_format=data_format)
+
+    assert loader.data_format == data_format
+
+
+@given(text_strategy)
+def test_simple_itk_uploader_raise_value_error(data_format: str):
+    '''
+    Check taht a value error is raised when SimpleITKUploader is 
+    init with an unsupported data format
+
+    Given:
+        - random strin as data format 
+    Then:
+        - init SimpleITKUploader with the invalid data format
+    Assert:
+        - value error is raised
+    '''
+
+    with pytest.raises(ValueError):
+        loader = SimpleITKUploader(data_format=data_format)
+
+
+def  test_simple_itk_uploader_correct_output_tuple_lenght():
+    '''
+    Check that the call method return a tuple of len 2, representing the input and 
+    the target images
+
+    Given:  
+        - No argument is required
+    Then:
+        - init SimpleITKUploader
+        - call the uploader providing a valid image and target paths
+    Assert:
+        - the returned tuple has len == 2
+    '''
+
+    loader = SimpleITKUploader()
+
+    samples = loader('test/test_images/test_image.nii', 'test/test_images/test_target.nii')
+    
+    assert len(samples) == 2
+
+
+def test_simple_itk_uploader_channels_last_correct_output_shape():
+    '''
+    Check that the red images have the correct shape, coherent with the channels_last 
+    data format
+
+    Given:
+        - no argument is required
+    Then:
+        - init the  SimpleITKUploader
+        - call the uploader providing a valid image and target paths
+    Assert:
+        - image and target shape are (64, 64, 1)
+    '''
+    
+    loader = SimpleITKUploader()
+
+    X, y = loader('test/test_images/test_image.nii', 'test/test_images/test_target.nii')
+
+    assert X.shape == (64, 64, 1)
+    assert y.shape == (64, 64, 1)
+
+
+
+
+
+def test_simple_itk_uploader_channels_first_correct_output_shape():
+    '''
+    Check that the red images have the correct shape, coherent with the channels_first 
+    data format
+
+    Given:
+        - no argument is required
+    Then:
+        - init the  SimpleITKUploader with channels_first as data_format
+        - call the uploader providing a valid image and target paths
+    Assert:
+        - image and target shape are (1, 64, 64)
+    '''
+    
+    loader = SimpleITKUploader(data_format='channels_first')
+
+    X, y = loader('test/test_images/test_image.nii', 'test/test_images/test_target.nii')
+
+    assert X.shape == (1, 64, 64)
+    assert y.shape == (1, 64, 64)
