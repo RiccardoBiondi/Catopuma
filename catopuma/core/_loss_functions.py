84e2442738b5670c19ff56bdd679c9f1beda844b
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2023-07-25 17:21:55 +0200

    [ENH] Add tversky loss implementation
    [TODO] Improve testing and documentation

--- catopuma/core/_loss_functions.py
+++ catopuma/core/_loss_functions.py
@@ -86,30 +86,74 @@ def f_score(y_true, y_pred, beta: float = 1., smooth: float = 1e-5, class_weight
     return score
 
 
-# TODO: rewrite this function to  handle all the cases considered also by the f1 score
-def tversky_loss(y_true, y_pred, alpha=.5, beta=.5, smooth=1e-6, dim_ordering='tf'):
+def tversky_score(y_true, y_pred, alpha: float = .5, beta: float = .5, smooth: float = 1e-5, class_weights: Union[List[float], float] = 1., 
+                  indexes: List[int] = None, per_image: bool = False, data_format: str = 'channels_last'):
+    '''
+    Function to compute the twersky metric . It will be a floating point value in [0., 1.].
+    This function allows to chose the conribution of precision and recall in loss computation, 
+    by setting the parameters alpha and beta. 
+    If alpha = .5 and beta = .5, it is the dice coefficient. If alpha = beta = 1, it is the 
+    Jaccard index.
 
-    if dim_ordering != 'tf':
-        y_true = K.permute_dimensions(y_true, (3, 0, 1, 2))
-        y_pred = K.permute_dimensions(y_pred, (3, 0, 1, 2))
+    Parameters
+    ----------
+    y_true:
+        ground truth, used as reference to compute thetversky score. 
+        The shape is (batch, height, width, channels) if  data_format is 'channel_last' (default value) or
+        (batch, channels,  height, width) if data format is 'channel_first'.
+
+    y_pred: 
+        prediction sample used to compute the thetversky score. Must be a floating point tensor with values in [0., 1.].
+        The shape is (batch, height, width, channels) if  data_format is 'channel_last' (default value) or
+        (batch, channels,  height, width) if data format is 'channel_first'.
+
+    alpha: 
+
+    beta:
+
+    smooth: float (default 1e-5)
+        smoothing value to use in the loss calculation to avoid division by zero.
+
+    class_weights: float or List of float
+        Contribution of each channel to the final score. Can be a single float or a list of float with the same len of the 
+        number of considered channels.
+    
+    indexes: List[int] deafult None
+        if provided, the list contaning the index of the channel to consider in the calculation of the loss.
+        If a channel index is repeated more than once, the loss is calsulated on the index as many time as the index
+        is repeated.
+        As default all the channels are considered in the order in which they are in y_true and y_pred.
+    
+    per_image bool (default false)
+        If true, the loss is calculated for each image and then averaged.
+
+    data_format: str (default 'channels_last')
+        specitfy the data format of the input samples.
+        Can be either 'channels_last', implying a format of (batch, height, width, channels);
+        or 'channels_first', implying a format of (batch, channels, height, width)
+
+    Return
+    ------
+    score: flaot
+        tversy score resulting form the computation. It is in [0, 1]
+    '''
+
+    gt = gather_channels(y_true, indexes=indexes, data_format=data_format)
+    pr = gather_channels(y_pred, indexes=indexes, data_format=data_format)
 
     y_true = K.cast(y_true, 'float32')
     y_pred = K.cast(y_pred, 'float32')
 
-    # clip to prevent NaN's and Inf's
-    y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+    # clip the values to avoid NaN's and Inf's
+    axes = get_reduce_axes(per_image=per_image, data_format=data_format)
 
-    y_true = y_true[..., 0] # assumption: I have a single channel image
-    y_pred = y_pred[..., 0] # assumption: I have a single channel image
+    # calculate score
+    tp = K.sum(gt * pr, axis=axes)
+    fp = K.sum(pr, axis=axes) - tp
+    fn = K.sum(gt, axis=axes) - tp
 
-    y_true = K.flatten(y_true)
-    y_pred = K.flatten(y_pred)
+    score = (tp + smooth) / (tp + alpha * fp + beta * fn + smooth)
 
-    # now compute the required quantities for the loss 
-    TP = K.sum((y_true * y_pred))
-    FP = K.sum((1 - y_true) * y_pred)
-    FN = K.sum(y_true * (1 - y_pred))
+    score = average(score, per_image, np.asarray(class_weights))
 
-    tversky = (TP + smooth) / (TP + alpha * FP + beta*FN + smooth)
-
-    return 1 - tversky
+    return score
--- catopuma/losses.py
+++ catopuma/losses.py
@@ -4,6 +4,7 @@ from typing import Union, Optional, List
 
 from catopuma.core.base_losses import BaseLoss
 from catopuma.core._loss_functions import f_score
+from catopuma.core._loss_functions import tversky_score
 
 __author__ = ['Riccardo Biondi']
 
@@ -48,3 +49,27 @@ class DiceLoss(BaseLoss):
         return 1. - f_score(y_true=y_true, y_pred=y_pred, beta=1., smooth=self.smooth,
                            indexes=self.class_indexes, class_weights=self.class_weights,
                            data_format=self.data_format, per_image=self.per_image)
+    
+
+class TverskyLoss(BaseLoss):
+    '''
+    '''
+
+    def __init__(self, smooth: float = 1e-5, alpha: float = .5, beta: float = .5, per_image: bool = False, class_weights: Union[float, List[float]] = 1.,
+                    class_indexes: Optional[List[int]] = None, data_format: str = 'channels_last', name: Optional[str] = None):
+        super().__init__(name=name)
+
+        self.smooth = smooth
+        self.alpha = alpha
+        self.beta = beta
+        self.per_image = per_image
+        self.class_weights = class_weights
+        self.class_indexes = class_indexes
+        self.data_format = data_format
+
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+
+        return 1. - tversky_score(y_true=y_true, y_pred=y_pred, alpha=self.alpha, beta=self.beta, smooth=self.smooth,
+                           indexes=self.class_indexes, class_weights=self.class_weights,
+                           data_format=self.data_format, per_image=self.per_image)
+    
