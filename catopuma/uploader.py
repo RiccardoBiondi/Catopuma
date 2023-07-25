9085a397b9f5d8559a035358346d9cf838af38fd
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2023-07-25 17:07:54 +0200

    [ENH] Added patch based lazy loader for medical images

--- catopuma/uploader.py
+++ catopuma/uploader.py
@@ -4,7 +4,7 @@ and tergetes.
 '''
 
 import os
-from typing import Tuple
+from typing import Tuple, NoReturn
 import numpy as np
 import SimpleITK as sitk
 import tensorflow as tf
@@ -30,7 +30,6 @@ class SimpleITKUploader(UploaderBase):
 
     def __call__(self, *path: Tuple[str]) -> Tuple[np.ndarray]:
         '''
-        Call method
         '''
 
         img = sitk.ReadImage(path[0]) # input path must be first
@@ -46,23 +45,112 @@ class SimpleITKUploader(UploaderBase):
         return img, tar
 
 
-class Patch2DLazyUploader(UploaderBase):
+class LazyPatchBaseUploader(UploaderBase):
     '''
+    Loader to peform a patch base lazy loading of medical images. 
+    The loader is based on the SimpleITK reader.
+
+    This implementation is inspired by the one of Gianluca Carlini:
+    https://github.com/GianlucaCarlini/Segmentation3D/blob/main/loaders/lazy_loaders.py
+
+    Parameters
+    ----------
+    patch_size: Tuple[int]
+        patch size. Must be specified for each image dimensions. 
+    threshold: float, (default -1.)
+        Threshold value to consider for patch sampling.
+        If the sum of non-zero pixels in the sampled patch is lower than
+        threshold, then another patch is sampled until the threshold condition is met.
+        As default each patch will be taken as valid
+    data_format:   str, (default 'channels_last')
+        data format of the returned images. Can be either 'channels_last' (i.e. (batch_size, w, h, channels))
+        or 'channels_first' (i.e., (batch_size, w, h, channels))
+
     '''
-
-    def __init__(self, patch_size: Tuple[int] = (16, 16), object_fraction: float = -1.,  data_format: str = 'channels_last'):
-
+    
+    def __init__(self, patch_size: Tuple[int], threshold: float = -1., data_format: str = 'channels_last') -> None:
+        
         super().__init__()
         if data_format in ['channels_first', 'channels_last']:
             self.data_format = data_format
         else:
             raise ValueError(f'{data_format} is not allowed')
-        
+            
         self.patch_size = patch_size
-        self.object_fraction = object_fraction
+        self.threshold = threshold
     
+    def __call__(self, *path: Tuple[str]) -> Tuple[np.array]:
+        
+        reader = sitk.ImageFileReader()
+        _ = reader.SetFileName(path[-1])
+        _ = reader.ReadImageInformation()
+        image_size = reader.GetSize()
+        
+        _ = self._checkConsistency(image_size)
+        
+        # insert the threshold case
+        condition = True
+        
+        while condition:
+            
+            patch_origin = self._samplePatchOrigin(image_size)
+            _ = reader.SetExtractIndex(patch_origin)
+            _ = reader.SetExtractSize(self.patch_size)
+            y = sitk.GetArrayFromImage(reader.Execute())[..., np.newaxis]
+            
+            if np.sum(y > 0.0) > self.threshold * np.prod(np.asarray(self.patch_size)):
+                condition = False
+            
+        _ = reader.SetFileName(path[0])
+        X = sitk.GetArrayFromImage(reader.Execute())[..., np.newaxis]
+        
+        if self.data_format == 'channels_first':
+            X = X.transpose(2, 0, 1)
+            y = y.transpose(2, 0, 1)
 
-    def __call__(self, *path: Tuple[str]) -> Tuple[np.asarray]:
+        return X, y
+            
+        
+        # extract and return (as batch and channel shaped)
+    
+    def _samplePatchOrigin(self, image_size: Tuple[int]) -> Tuple[int]:
         '''
+        Sample the patch origin according to a uniform distribution ensuring 
+        that all the patch is inside the the image
+        
+        Parameter
+        ---------
+            image_size: Tuple[int]
+                size of the image from which extract the patch origin
+        Return
+        ------
+            origin: Tuple[int]
+                patch origin
         '''
-        pass
+        origin = [np.random.randint(0, max(i - p, 1)) for i,p in zip(image_size, self.patch_size)]
+        
+        return origin
+
+    def _checkConsistency(self, image_size: Tuple[int]) -> NoReturn:
+        '''
+        Check the image from which extract the patch have the same dimension of the 
+        specified patch size. Also check that the patch size is lower than the image
+        size, i.e. the patch is smaller or equal than the image.
+        
+        Parameters
+        ----------
+        image_size: Tuple[int]
+            size of the image from which extract the patch
+        
+        Raise
+        -----
+        ValueError, is the image_size and the patch_size are not consistent.
+        '''
+        
+        
+        if len(image_size) != (len(self.patch_size)):
+            
+            raise ValueError(f'image and pathc size must have the same dimenson: {len(image_size)} != {len(self.patch_size)}')
+            
+        if np.any(np.asarray(image_size) < np.asarray(self.patch_size)):
+            raise ValueError(f'Patch must be contained inside the image: {image_size} < {self.patch_size}')
--- test/test_uploader.py
+++ test/test_uploader.py
@@ -1,17 +1,14 @@
 import pytest
 import hypothesis.strategies as st
-from hypothesis import given, settings
+from hypothesis import given, settings, assume
 
-
+import numpy as np
 from catopuma.uploader import SimpleITKUploader
-
+from catopuma.uploader import LazyPatchBaseUploader
 ALLOWED_DATA_FORMATS =   ('channels_first', 'channels_last')
 
-legitimate_chars = st.characters(whitelist_categories=('Lu', 'Ll'),
-                                 min_codepoint=65, max_codepoint=90)
-
-text_strategy = st.text(alphabet=legitimate_chars, min_size=1,
-                        max_size=15)
+legitimate_chars = st.characters(whitelist_categories=('Lu', 'Ll'), min_codepoint=65, max_codepoint=90)
+text_strategy = st.text(alphabet=legitimate_chars, min_size=1, max_size=15)
 
 def test_simple_itk_uploader_default_init():
     '''
@@ -130,3 +127,217 @@ def test_simple_itk_uploader_channels_first_correct_output_shape():
 
     assert X.shape == (1, 64, 64)
     assert y.shape == (1, 64, 64)
+
+
+def test_lazy_patch_base_uploader_default_init():
+    '''
+    Test that all the dafault parametner are set correctly
+
+    Given:
+        - No argument is required
+    Then:
+        - init the LazyPatchBaseUploade with default arguments
+    Assert:
+        all the class members are init to their default values
+    '''
+
+    loader = LazyPatchBaseUploader((16, 16))
+
+    assert loader.data_format == 'channels_last'
+    assert loader.threshold == -1
+
+
+@given(st.sampled_from(ALLOWED_DATA_FORMATS), st.floats(0., 1.), st.lists(st.integers(16, 32), min_size=2, max_size=3 ))
+def test_lazy_patch_base_uploader_init(data_format: str, threshold: float, patch_size):
+    '''
+    Check that LazyPatchBaseUploader arguments are correctly instantiated
+
+    Given:
+        - valid data format
+        - valid threshold value
+        - valid patch size
+    Then:
+        - init LazyPatchBaseUploader
+    Assert:
+        - LazyPatchBaseUploader data format is equal to the input one
+    '''
+    
+    loader = LazyPatchBaseUploader(patch_size=patch_size, threshold=threshold, data_format=data_format)
+
+    assert np.all(np.asarray(loader.patch_size) == np.asarray(patch_size))
+    assert loader.threshold == threshold
+    assert loader.data_format == data_format
+
+
+@given(text_strategy)
+def test_lazy_patch_base_uploader_raise_value_error_for_wrong_data_format(data_format: str):
+    '''
+    Check taht a value error is raised when LazyPatchBaseUploader is init with an unsupported data format
+
+    Given:
+        - random strin as data format 
+    Then:
+        - init LazyPatchBaseUploader with the invalid data format
+    Assert:
+        - value error is raised
+    '''
+
+    with pytest.raises(ValueError):
+        loader = LazyPatchBaseUploader(patch_size=(16, 16), data_format=data_format)
+
+@given(st.integers(2, 3), st.integers(8, 64), st.integers(16, 64))
+def test_lazy_patch_base_uploader_check_consistency_patch_outside_image_raise_value_error(dimension, image_size, patch_size):
+    '''
+    Check that the _checkConsistency method raise ValueError when the required patch dimension 
+    is bigger than the input image
+
+    Given:
+        - dimension
+        - image_size
+        - patch_size
+    Then:
+        - init  LazyPatchBaseUploader
+        - call the _checkConsistency method
+    Assert:
+        - value error is raised
+    '''
+    assume(image_size < patch_size)
+    
+    im_size = dimension * [image_size]
+    pc_size = dimension * [patch_size]
+
+
+    loader = LazyPatchBaseUploader(patch_size=pc_size)
+
+    with pytest.raises(ValueError):
+
+        _ = loader._checkConsistency(image_size=im_size)
+
+
+@given(st.integers(2, 3), st.integers(2, 3))
+def test_lazy_patch_base_uploader_check_consistency_mismatch_dimension_raise_value_error(patch_dim, image_dim):
+    '''
+    Check that the checkConsistency metod raise value error when patch size and image size have diferent dimension
+
+    Given:
+        - patch dimension
+        - image dimension
+    Then:
+        - assume patch_dimesion != image_dimension
+        - create patch and image sizes
+        - init  LazyPatchBaseUploader
+        - call the _checkConsistencyMethod
+    Assert:
+        - _checkConsistency method raise value error
+    '''
+
+    assume(patch_dim != image_dim)
+
+    patch_size = patch_dim * [16]
+    image_size = image_dim * [64]
+
+    loader = LazyPatchBaseUploader(patch_size=patch_size)
+
+    with pytest.raises(ValueError):
+        _ = loader._checkConsistency(image_size=image_size)
+
+
+@given(st.lists(st.integers(16, 32), min_size=2, max_size=3 ))
+def test_lazy_patch_base_uploader_sample_patch_origin_is_zero(patch_size):
+    '''
+    Check that the sampled patch origin is alwaise zero when the patch_size and the image size are the 
+    same.
+
+    Given:
+        - patch_size
+    Then:
+        - Init the LazyPatchBaseUploader
+        - call the _samplePatchOrigin method with image size equal to the patch size
+    Assert:
+        - sampled patch origin is 0
+    '''
+
+    loader = LazyPatchBaseUploader(patch_size=patch_size)
+
+    origin = loader._samplePatchOrigin(patch_size)
+    
+    zeros = len(origin) * [0]
+
+    assert np.all(np.asarray(zeros) == np.asarray(origin))
+
+
+
+@given(st.integers(2, 3), st.integers(4, 16), st.integers(32, 128))
+def test_lazy_patch_base_uploader_sample_patch_origin_is_in_image(dimensions, patch_size, image_size):
+    '''
+    Check that the sampled patch origin, and the whole patch, are inside the image
+
+    Given:
+        - dimensions 
+        - patch_size
+        - image_size
+    Then:
+        - build image and patch sizes
+        - Init the LazyPatchBaseUploader
+        - call the _samplePatchOrigin
+    Assert:
+        - patch origin and upper indexes are inside the image
+    '''
+
+    patch_size = dimensions * [patch_size]
+    image_size = dimensions * [image_size]
+
+    loader = LazyPatchBaseUploader(patch_size=patch_size)
+
+    origin =  loader._samplePatchOrigin(image_size=image_size)
+    
+    upper_index = np.asarray(origin) + np.asarray(patch_size)
+    assert np.all(np.asarray(origin) >= np.zeros(dimensions))
+    assert np.all(np.asarray(origin) < np.asarray(image_size))
+    assert np.all(np.asarray(upper_index) < np.asarray(image_size))
+
+
+@given(st.integers(4, 16))
+def test_lazy_patch_base_uploader_channels_last_correct_output_shape(patch_size):
+    '''
+    Check that the red images have the correct shape, coherent with the channels_last 
+    data format and the specified patch size
+
+    Given:
+        - patch_size
+    Then:
+        - init the  SimpleITKUploader
+        - call the uploader providing a valid image and target paths
+    Assert:
+        - image and target shape are (patch_size, 1)
+    '''
+    
+    loader = LazyPatchBaseUploader(patch_size=(patch_size, patch_size))
+
+    X, y = loader('test/test_images/test_image.nii', 'test/test_images/test_target.nii')
+
+    assert X.shape == (patch_size, patch_size, 1)
+    assert y.shape == (patch_size, patch_size, 1)
+
+
+@given(st.integers(4, 16))
+def test_lazy_patch_base_uploader_channels_first_correct_output_shape(patch_size):
+    '''
+    Check that the red images have the correct shape, coherent with the channels_first 
+    data formatand the specified patch size
+
+    Given:
+        - patch_size
+    Then:
+        - init the  SimpleITKUploader with channels_first as data_format
+        - call the uploader providing a valid image and target paths
+    Assert:
+        - image and target shape are (1, patch_size)
+    '''
+    
+    loader = LazyPatchBaseUploader(patch_size=(patch_size, patch_size), data_format='channels_first')
+
+    X, y = loader('test/test_images/test_image.nii', 'test/test_images/test_target.nii')
+
+    assert X.shape == (1, patch_size, patch_size)
+    assert y.shape == (1, patch_size, patch_size)
