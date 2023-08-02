bafbed405513d34b1021bb443e94d7d4330a15db
RiccardoBiondi <riccardo.biondi7@unibo.it>
2023-08-02 21:28:25 +0200

    [ENH] Improved framework management

--- catopuma/__init__.py
+++ catopuma/__init__.py
@@ -3,10 +3,11 @@
 #from catopuma.losses import *
 #from catopuma.uploader import *
 import os
-#import importlib
+import sys
+import importlib
 from catopuma.__version__ import __version__
 import catopuma.core.framework as fw
-from typing import Tuple
+from typing import Iterable
 
 __author__ = ['Riccardo Biondi']
 __email__ = ['riccardo.biondi7@unibo.it']
@@ -15,8 +16,25 @@ __email__ = ['riccardo.biondi7@unibo.it']
 
 # now start the injection of the backend, to support both torch and keras (both keraas and tf.keras) implementations
 
+def set_available_frameworks():
+    '''
+    Retrieve which of the supported frameworks are available in the system.
 
+    '''
+    fw._AVAILABLE_FRAMEWORKS = [k for  k, v in fw._FRAMEWORK_LUT.items() if importlib.find_loader(v) is not None]
 
+def available_frameworks() -> Iterable[str]:
+    '''
+    Get the list of availble catopuma frameworks.
+
+    Returns
+    -------
+    list of str
+        list of available frameworks
+    '''
+    
+    return fw._AVAILABLE_FRAMEWORKS
+ 
 def framework() -> str:
     '''
     Return the current framework
@@ -26,7 +44,8 @@ def framework() -> str:
 
 def set_framework(name: str) -> None:
     '''
-    Set the framework to use as backwend.
+    Set the framework to use as backend.
+    If a framework is already set, it will be removed and the new one will be set.
 
     Parameters
     ----------
@@ -39,59 +58,31 @@ def set_framework(name: str) -> None:
         ImportError: in case framework is not installed.
     '''
 
-    if name not in fw._SUPPORTED_FRAMEWORKS:
-        raise ValueError(f'The specified framework {name} is not valid. Allowed frameworks are: {fw._SUPPORTED_FRAMEWORKS}')
-    
+    if name not in fw._AVAILABLE_FRAMEWORKS:
+        raise ValueError(f'The specified framework {name} is not available. Available frameworks are: {fw._AVAILABLE_FRAMEWORKS}')
     
     fw._FRAMEWORK_NAME = name
 
+    # Then import the requested framework
     if fw._FRAMEWORK_NAME == fw._KERAS_FRAMEWORK_NAME:
-        import keras
 
+        import keras
         fw._FRAMEWORK_BACKEND = keras.backend
         fw._FRAMEWORK = keras
     
     elif fw._FRAMEWORK_NAME == fw._TF_KERAS_FRAMEWORK_NAME:
 
         import tensorflow.keras as keras
-
         fw._FRAMEWORK_BACKEND = keras.backend
         fw._FRAMEWORK = keras
 
-#spam_loader = importlib.find_loader('spam')
-#found = spam_loader is not None
+    elif fw._FRAMEWORK_NAME == fw._TORCH_FRAMEWORK_NAME:
 
-"""
-def  set_supported_frameworks() -> None:
-    '''
-    Function to set the catopuma.core.framework._SUPPORTED_FRAMEWORKS according to 
-    the installed libraries; i.e. check if tensorflow, keras and pytorch are available.
-    '''
-
-    sup_frameworks = []
-
-    try: 
-        import keras
-        sup_frameworks.append(fw._KERAS_FRAMEWORK_NAME)
-    except:
-        pass
-
-    try: 
-        import tensorflow.keras
-        sup_frameworks.append(fw._TF_KERAS_FRAMEWORK_NAME)
-    except:
-        pass
-
-    #try: 
-    import torch
-    #    sup_frameworks.append(fw._TORCH_FRAMEWORK_NAME)
-    #except:
-    #    pass
-
-    fw._SUPPORTED_FRAMEWORKS = sup_frameworks
-"""
+        import torch
+        fw._FRAMEWORK_BACKEND = torch
+        fw._FRAMEWORK = torch
 
 # now set the frameworks
-#set_supported_frameworks()
+set_available_frameworks()
 _framework = os.environ.get('CATOPUMA_FRAMEWORK', fw._DEFAULT_FRAMEWORK_NAME)
 set_framework(_framework)
--- catopuma/core/framework.py
+++ catopuma/core/framework.py
@@ -1,22 +1,50 @@
 '''
 Core module to manage the framework settings. Up to now the available frameworks are keras and tensorflow.keras;
 but I have planned also to add torch, in order to have a repo to rule them all.
+The framework is set by the user and it is used to import the correct backend module.
+The backend module is then used to perform the actual computation.
 '''
-from typing import Tuple
+import sys
+from typing import Iterable, Dict
 
-
+__author__ = ['Riccardo Biondi']
+__email__ = ['riccardo.biondi7@unibo.it']
 
 
 _KERAS_FRAMEWORK_NAME: str = 'keras'
 _TF_KERAS_FRAMEWORK_NAME: str = 'tf.keras'
 _TORCH_FRAMEWORK_NAME: str = 'torch'
 
+
+_FRAMEWORK_LUT: Dict[str, str] = {
+        _KERAS_FRAMEWORK_NAME : 'keras',
+        _TF_KERAS_FRAMEWORK_NAME : 'tensorflow.keras',
+        _TORCH_FRAMEWORK_NAME: 'torch'
+    }
+
+
 global _FRAMEWORK_BACKEND, _FRAMEWORK, _FRAMEWORK_NAME, _SUPPORTED_FRAMEWORKS
-_SUPPORTED_FRAMEWORKS: Tuple[str]  = [_KERAS_FRAMEWORK_NAME, _TF_KERAS_FRAMEWORK_NAME]
+_AVAILABLE_FRAMEWORKS: Iterable[str]  = [_KERAS_FRAMEWORK_NAME]
 
 
 _FRAMEWORK_NAME = None
 _FRAMEWORK_BACKEND = None
 _FRAMEWORK = None
 
-_DEFAULT_FRAMEWORK_NAME: str = _KERAS_FRAMEWORK_NAME
+_DEFAULT_FRAMEWORK_NAME: str = _KERAS_FRAMEWORK_NAME#AVAILBLE_FRAMEWORKS[0]
+
+
+def clear_framework(framework_name: str): 
+    '''
+    Clear the framework settings and remove the backend module from the sys.modules.
+
+    Parameters
+    ----------
+    framework_name: str
+        name of the framework to clear. Can be: ``keras``, ``tf.keras`` or ``torch``
+    '''
+    package_name = _FRAMEWORK_LUT[framework_name]
+    loaded_package_modules = [module_name for module_name in sys.modules.keys() if package_name in module_name]
+    for module_name in loaded_package_modules:
+        sys.modules.pop(module_name, None)
+    _FRAMEWORK_NAME = None
