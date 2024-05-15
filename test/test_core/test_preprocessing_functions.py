8d0ee06cdca6813d4a384dbc186898107844962d
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2024-05-15 17:10:42 +0200

    [ENH][TEST] Add tests for the preprocessing functions

--- test/test_core/test_preprocessing_functions.py
+++ test/test_core/test_preprocessing_functions.py
@@ -10,7 +10,9 @@ from catopuma.core._preprocessing_functions import standard_scaler
 from catopuma.core._preprocessing_functions import robust_scaler
 from catopuma.core._preprocessing_functions import min_max_scaler
 from catopuma.core._preprocessing_functions import identity
+from catopuma.core._preprocessing_functions import unpack_labels
 
+ALLOWED_DATA_FORMATS =   ('channels_first', 'channels_last')
 
 @given(st.integers(1, 8), st.integers(1, 4))
 def test_identity_return_the_same_image(batch_size: int, channels: int):
@@ -293,7 +295,7 @@ def test_robust_scaler(batch_size: int, n_channels: int):
 
     median = np.median(scaled)
     iqr = np.subtract(*np.percentile(scaled, [75, 25]))
-                      
+
     assert np.isclose(median, 0.)
     assert np.isclose(iqr, 1.)
 
@@ -326,7 +328,7 @@ def test_robust_scaler_per_image(batch_size: int, n_channels: int):
 
     median = np.median(scaled, axis=(1, 2, 3))
     iqr = np.subtract(*np.percentile(scaled, [75, 25], axis=(1, 2, 3)))
-                      
+
     assert np.all(np.isclose(median, 0.))
     assert np.all(np.isclose(iqr, 1.))
 
@@ -359,7 +361,7 @@ def test_robust_scaler_per_channel(batch_size: int, n_channels: int):
 
     median = np.median(scaled, axis=(0, 1, 2))
     iqr = np.subtract(*np.percentile(scaled, [75, 25], axis=(0, 1, 2)))
-                      
+
     assert np.all(np.isclose(median, 0.))
     assert np.all(np.isclose(iqr, 1.))
 
@@ -391,6 +393,164 @@ def test_robust_scaler_per_image_per_channel(batch_size: int, n_channels: int):
 
     median = np.median(scaled, axis=(1, 2))
     iqr = np.subtract(*np.percentile(scaled, [75, 25], axis=(1, 2)))
-                      
+
     assert np.all(np.isclose(median, 0.))
     assert np.all(np.isclose(iqr, 1.))
+
+
+@given(st.integers(2, 8), st.integers(2, 10))
+@settings(max_examples=5,
+        deadline=None,
+        suppress_health_check=(HC.too_slow,))
+def test_unpack_labels_return_binary_image_channels_last(batch_size: int, label_value: int):
+    '''
+    Test that the image returned by the unpack image has only 0. and 1. values
+
+    Given:
+        - batch size
+        - label to select
+    Then:
+        - generate random labelmap image
+        - call the unpack_labels (data_format='channels_last')
+    Assert:
+        - image unique values are only 0 and 1.
+    '''
+    image = (label_value * (np.random.rand(batch_size, 64, 64, 1))).astype(np.uint8)
+
+    y = unpack_labels(image, data_format='channels_last', labels=[label_value - 1])
+
+    uniques = np.unique(y)
+
+    assert len(uniques) == 2
+    assert 0 in uniques
+    assert 1 in uniques
+
+
+@given(st.integers(2, 8), st.integers(2, 10))
+@settings(max_examples=5,
+        deadline=None,
+        suppress_health_check=(HC.too_slow,))
+def test_unpack_labels_return_binary_image_channels_first(batch_size: int, label_value: int):
+    '''
+    Test that the image returned by the unpack image has only 0. and 1. values
+
+    Given:
+        - batch size
+        - label to select
+    Then:
+        - generate random labelmap image
+        - call the unpack_labels (data_format='channels_first')
+    Assert:
+        - image unique values are only 0 and 1.
+    '''
+    image = (label_value * (np.random.rand(batch_size, 1, 64, 64))).astype(np.uint8)
+
+    y = unpack_labels(image, data_format='channels_first', labels=[label_value - 1])
+
+    uniques = np.unique(y)
+
+    assert len(uniques) == 2
+    assert 0 in uniques
+    assert 1 in uniques
+
+@given(st.integers(2, 8), st.integers(1, 5))
+@settings(max_examples=5,
+        deadline=None,
+        suppress_health_check=(HC.too_slow,))
+def test_unpack_labels_return_correct_shape_channels_last(batch_size: int, n_labels: int):
+    '''
+    Test that the image returned by the unpack image has a shape equal to the selected number of labels
+
+    Given:
+        - batch size
+        - number of labels to select
+    Then:
+        - generate random labelmap image
+        - call the unpack_labels (data_format='channels_last')
+    Assert:
+        - the result image shape is (batch_size, 64, 64, number of labels)
+    '''
+    image = (n_labels * (np.random.rand(batch_size, 64, 64, 1))).astype(np.uint8)
+
+    y = unpack_labels(image, data_format='channels_last', labels=list(range(0, n_labels)))
+
+    assert y.shape == (batch_size, 64, 64, n_labels)
+
+
+@given(st.integers(2, 8), st.integers(1, 5))
+@settings(max_examples=5,
+        deadline=None,
+        suppress_health_check=(HC.too_slow,))
+def test_unpack_labels_return_correct_shape_channels_first(batch_size: int, n_labels: int):
+    '''
+    Test that the image returned by the unpack image has a shape equal to the selected number of labels
+
+    Given:
+        - batch size
+        - number of labels to select
+    Then:
+        - generate random labelmap image
+        - call the unpack_labels (data_format='channels_first')
+    Assert:
+        - the result image shape is (batch_size, number of labels, 64, 64)
+    '''
+    image = (n_labels * (np.random.rand(batch_size, 1, 64, 64))).astype(np.uint8)
+
+    y = unpack_labels(image, data_format='channels_first', labels=list(range(0,     n_labels)))
+
+    assert y.shape == (batch_size, n_labels, 64, 64)
+
+
+@given(st.integers(2, 8), st.integers(1, 5))
+@settings(max_examples=5,
+        deadline=None,
+        suppress_health_check=(HC.too_slow,))
+def test_unpack_labels_return_correct_label_channels_last(batch_size: int, n_labels: int):
+    '''
+    Test that each channel of the resulting image is equal to the correct object
+
+    Given:
+        - batch size
+        - number of labels to select
+    Then:
+        - generate random labelmap image
+        - call the unpack_labels (data_format='channels_last')
+    Assert:
+        - Test that each channel correspond to the correct label
+    '''
+
+    image = (n_labels * (np.random.rand(batch_size, 64, 64, 1))).astype(np.uint8)
+
+    y = unpack_labels(image, data_format='channels_last', labels=list(range(0, n_labels)))
+
+
+    for i in range(0, n_labels):
+
+        assert np.all(y[..., i] == (image == i).astype(np.uint8).reshape(batch_size, 64, 64))
+
+@given(st.integers(2, 8), st.integers(1, 5))
+@settings(max_examples=5,
+        deadline=None,
+        suppress_health_check=(HC.too_slow,))
+def test_unpack_labels_return_correct_label_channels_first(batch_size: int, n_labels: int):
+    '''
+    Test that each channel of the resulting image is equal to the correct object
+
+    Given:
+        - batch size
+        - number of labels to select
+    Then:
+        - generate random labelmap image
+        - call the unpack_labels (data_format='channels_first')
+    Assert:
+        - Test that each channel correspond to the correct label
+    '''
+
+    image = (n_labels * (np.random.rand(batch_size, 1, 64, 64))).astype(np.uint8)
+
+    y = unpack_labels(image, data_format='channels_first', labels=list(range(0, n_labels)))
+
+
+    for i in range(0, n_labels):
+
+        assert np.all(y[:, i] == (image == i).astype(np.uint8).reshape(batch_size, 64, 64))
