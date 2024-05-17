b840259719a05c55f76f33e81e9c19e763f0b2d5
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2024-05-17 08:51:56 +0200

    [ENH] Add the image feeder (as torch.utils.data.Dataset) specific for the torch workflow

--- catopuma/torch/feeder.py
+++ catopuma/torch/feeder.py
@@ -8,6 +8,9 @@ import catopuma
 from catopuma.uploader import SimpleITKUploader
 from catopuma.core.base import UploaderBase, PreProcessingBase, DataAgumentationBase
 
+from catopuma.core.__framework import _FRAMEWORK_BASE
+from catopuma.core.__framework import _DATA_FORMAT
+
 
 __author__ = ['Riccardo Biondi']
 __email__ = ['riccardo.biondi7@unibo.it']
@@ -15,15 +18,72 @@ __all__ = []
 
 
 
-class FeederDataset(torch.utils.data.Dataset):
+class ImageFeederOnTheFly(torch.utils.data.Dataset):
+    '''
+    Class implementing the Dataset to load the images to the deep Learning model. 
+    This class is implemented to load each image at run time, saving memory when the whole 
+    sample is too long and too heavy.
+    It also has the possibility to augment and preprocess the input data and targets on the fly, 
+    before presenting the image to DataLoader and the Model.
+    It also allows to specify different readers for different kind of images and for different 
+    reading modalities.
+    Parameters
+    ----------
+    paths: List[str]
+        list of strings. Each list must contain the paths to the image to load.
+        At least two lists ,ust be provided, one for imagesand one for targets.
+        The last list provided will be considered as the taget paths list, all the 
+        others as the input image paths.
+    uploader: UploaderBase (default SimpleITKUploader)
+        the uploader used to load image and targets. Must return a X, y pair where X, y and the input and the 
+        target images. Custom uploader must inherit from UploaderBase
+    preprocessing: PreProceessingBase (default None)
+        if specified, the class specifying the preprocessing routine. Must return a pair X, y and inherit from
+        PreProcessingBase
+    augmentation_strategy: DataAugmentationBase (default None)
+        class implemeting the strategies to augment the data on the fly.
+        Must return a pair X, y and inherit from DataAgumentationBase
 
-    def __init__(self, img_paths: List[str], target_paths: List[str],
-                 uploader: UploaderBase = SimpleITKUploader(), 
-                 preprocessing: Optional[PreProcessingBase] = None,
-                 augmentation_strategy: Optional[DataAgumentationBase] = None):
+    Example
+    -------
+    >>> import os
+    >>> os.environ['CATOPUMA_FRAMEWORK'] = 'torch'
+    >>> import catopuma
+    >>> from catopuma.uploader import SimpleITKUploader
+    >>> from catopuma.preprocessing import PreProcessing
+    >>> import catopuma.feeder as feeder
+    >>> from torch.utils.data import DataLoader
+    >>> 
+    >>> import matplotlib.pyplot as plt
+    >>>
+    >>> # define the paths to the input image and the target images
+    >>> img_paths = ['img1.nii', 'img2.nii', 'img3.nii']
+    >>> tar_paths = ['tar1.nii', 'tar2.nii', 'tar3.nii']
+    >>> 
+    >>> # define the pre-processing to apply to the image (rescale the image into [0., 1.])
+    >>> preprocessing = PreProcessing(standardizer='minmax', target_standardizer='identity')
+    >>> feed = feeder(img_path, tar_path, preprocessing=preprocessing)
+    >>> 
+    >>> # show the loaded images
+    >>> for i in range(0, 3):
+    >>>     X, y = feed[i]
+    >>>     fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
+    >>>     _ = ax0.imshow(X, cmap='gray')
+    >>>     _ = ax1.imshow(y, cmap='gray')
+    >>>
+    >>> # now define the DataLoader to feed the batch to the model
+    >>> train_dataloader = DataLoader(feed, batch_size=2, shuffle=True)
+    '''
+
+    def __init__(self, *paths,
+                uploader: UploaderBase = SimpleITKUploader(), 
+                preprocessing: Optional[PreProcessingBase] = None,
+                augmentation_strategy: Optional[DataAgumentationBase] = None):
         
-        self.img_paths = np.asarray(img_paths)
-        self.tar_paths = np.asarray(target_paths)
+        self.paths = paths
+
+        self._checkConsistency()
+
         self.uploader = uploader
         self.preprocessing = preprocessing
         self.augmentation_strategy = augmentation_strategy
@@ -40,7 +100,7 @@ class FeederDataset(torch.utils.data.Dataset):
             number of full batches
         '''
 
-        return len(self.img_paths)
+        return len(self.paths[0])
 
     def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray]:
         '''
@@ -61,9 +121,11 @@ class FeederDataset(torch.utils.data.Dataset):
         # get the list of index for the batch
 
         # load the image and the labels
-        X, y = self.uploader(self.img_paths[idx], self.tar_paths[idx])#list(zip(*[self.uploader(img, tar) for img, tar in zip(self.img_paths[idxs], self.tar_paths[idxs])]))
-        X = np.asarray(X)
-        y = np.asarray(y)
+        X, y = self.uploader(*[p[idx] for p in self.paths])
+        # add batch dimension to keep it consistent with the pre-processing step
+        # it will be removed at the end of the process
+        X = np.asarray(X)[np.newaxis]
+        y = np.asarray(y)[np.newaxis]
 
         # now apply eventual data augmentation 
         if self.augmentation_strategy is not None:
@@ -73,117 +135,24 @@ class FeederDataset(torch.utils.data.Dataset):
         if self.preprocessing is not None:
             X, y = self.preprocessing(X, y)
 
+        X = np.squeeze(X, 0)
+        y = np.squeeze(y, 0)
+
         return torch.Tensor(X.astype(np.float32)), torch.Tensor(y.astype(np.float32))
 
-
-
-class ImageFeederOnTheFly(torch.utils.data.DataLoader):
-    '''
-    '''
-    
-    def __init__(self, img_paths: List[str], target_paths: List[str], batch_size: int = 8, shuffle: bool = True,
-                uploader: UploaderBase = SimpleITKUploader(), 
-                preprocessing: Optional[PreProcessingBase] = None,
-                augmentation_strategy: Optional[DataAgumentationBase] = None):
-        
-        self.batch_size = batch_size
-
-        # Here I am defining the dataset inside the init of the ImageFeederOnTheFly object. 
-        # This is not so maintainable, but could simplify the whole repo usage,
-        # However, consider to implement dependency inversion and injection pattern also in this case.
-        # Howerr It could not be necessasy required
-        self.dataset = FeederDataset(img_paths=img_paths, target_paths=target_paths,
-                                     uploader=uploader, preprocessing=preprocessing, augmentation_strategy=augmentation_strategy)
-
-        _ = self._checkConsistency()
-
-        self.indexes = np.arange(0, len(img_paths), 1)
-        self.shuffle = shuffle
-        self.img_path = img_paths
-        self.target_paths = target_paths
-        self.uploader = uploader
-        self.preprocessing = preprocessing
-        self.augmentation_strategy = augmentation_strategy
-
-        # and thes shuffle if specified
-        _ = self.on_epoch_end()
-    
     def _checkConsistency(self) -> NoReturn:
         '''
         Check the consistency of the provided init arguments:
-            - len(img_paths) == len(tar_paths)
-            - len(img_paths) >= batch_size
+            - len paths >= 2 (at least a path to a single input image and to a target image
+            should be provided)
+            - all the paths lists must have the same lenght
 
         If one of those requirements is not met, it will raise a value error.
         '''
 
-        if len(self.dataset.img_paths) != len(self.dataset.tar_paths):
-            raise ValueError(f'len of image paths must be the same of the targets: {len(self.dataset.img_paths)} != {len(self.dataset.tar_paths)}')
-        if len(self.dataset.img_paths) < self.batch_size:
-            raise ValueError(f'len of imagese must be at least equal to the batch size: {len(self.dataset.img_paths)} < {self.batch_size}')
-
-    def __len__(self) -> int:
-        '''
-        Return the number batches.
-        This function returns only the numbers of complete batches.
-        Batches with a number of examples lower than batch_size are not considered
-
-        Return
-        ------
-        len: int
-            number of full batches
-        '''
-
-        return len(self.dataset) // self.batch_size
-
-    def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray]:
-        '''
-        Return the batch corresponding of idx.
-
-        Parameters
-        ----------
-        idx: int
-            The index corresponding to the desired batch.
-            Must be lower than the number of full batches
-
-        Return
-        ------
-        X, y: Tuple[np.ndarray, np.ndarray]
-            X input images, y corresponding target of the batch
-        '''
-
-        # get the list of index for the batch
-        idxs = self.indexes[self.batch_size * idx: self.batch_size * (idx + 1)]
-
-        return self.dataset[idxs]
- 
-    def on_epoch_end(self):
-        '''
-        Function called at the end of each epoch.
-        If self.shuffle is true, it will shuffle the images.
-        '''
-
-        if self.shuffle is True:
-            np.random.shuffle(self.indexes)
-
-
-    #
-    # Define some calss methods to init the class in different ways, like from a directory,
-    # from a dataframe or from an existing dataset
-    #
-
-    @classmethod
-    def flow_from_directory(cls):
-        pass
-
-    @classmethod
-    def from_dataset(cls):
-        pass
-
-    @classmethod
-    def flow_from_dataframe(cls):
-        pass
-
-    @classmethod
-    def flow_from_textfile(cls):
-        pass
+        if len(self.paths) < 2:
+            raise ValueError(f'At least two list of paths must be provided. Provided {len(self.paths)}')
+        
+        for path in self.paths[1:]:
+            if len(self.paths[0]) != len(path):
+                raise ValueError(f'All the provided image paths must be the same: {len(self.paths[0])} != {len(path[0])}')
--- test/test_feeder.py
+++ test/test_feeder.py
@@ -1,7 +1,5 @@
 import catopuma
 import catopuma.core.__framework as fw
- 
-
 
 import pytest
 import hypothesis.strategies as st
@@ -211,6 +209,101 @@ class TestImageFeederTensorflow:
 
 @pytest.mark.skipif(fw._FRAMEWORK_NAME != 'torch', reason="Test only works with torch frameworks")
 class TestFeederTorch:
-      
-      def test_placeholder_torch(self):
-            assert True
+
+    def test_image_feeder_on_the_fly_default_init(self):
+        '''
+        Check if all the dafault parameters are correctly setted
+        Given:
+            - no argument is required
+        Then:
+            - init the ImageFeederOnTheFly. Use a placeholder list of path
+        Assert:
+            - all arguments are equal to the default ones
+        '''
+        # create the palceholder paths
+        img_paths = 8 * ['test/test_images/test_image.nii']
+        tar_paths = 8 * ['test/test_images/test_target.nii']
+        imfeeder = feeder.ImageFeederOnTheFly(img_paths, tar_paths)
+        assert isinstance(imfeeder.uploader, SimpleITKUploader)
+        assert imfeeder.preprocessing is None
+        assert imfeeder.augmentation_strategy is None
+        # check indexes is correctly initialized
+
+    @given(st.integers(5, 100), st.integers(10, 100))
+    def test_image_feeder_on_the_fly_raise_value_error_path_different_lenght(self, image_path_size, target_path_size):
+        '''
+        Chech that the image feeder raise value error when different lenght of
+        image and target paths are provided
+        Given:
+            - lenght of image paths
+            - lenght of target paths
+        Then:
+            - create the image and target paths list
+            - init the image feeder
+        Assert:
+            - value error is raised
+        '''
+        assume(image_path_size != target_path_size)
+        img_paths = image_path_size * ['test/test_images/test_image.nii']
+        img2_paths = image_path_size * ['test/test_images/test_image.nii']
+        tar_paths = target_path_size * ['test/test_images/test_target.nii']
+
+        with pytest.raises(ValueError):
+            imfeeder = feeder.ImageFeederOnTheFly(img_paths, img2_paths, tar_paths)
+
+
+    @given(st.integers(1, 8), st.integers(1, 100))
+    def test_image_feeder_on_the_fly_len(self, n_channels, number_of_images):
+        '''
+        Check that the len of the feeder (i.e. the number of images in the dataset) is number_of_images when 
+        the path len number_of_images.
+        
+        Given:
+            - number_of_images
+            - number of image channels
+        Then:
+            - image_paths
+            - init the image feeder
+        Assert:
+            - len of the feeder is equal to the provided number of batches
+        '''
+        # generate the noise
+
+        img_paths = number_of_images * ['test/test_images/test_image.nii']
+        channels_paths = n_channels * [img_paths]
+        tar_paths = number_of_images * ['test/test_images/test_target.nii']
+        imfeeder = feeder.ImageFeederOnTheFly(*channels_paths, tar_paths)
+        assert len(imfeeder) == number_of_images
+
+
+    @given(st.integers(1, 5), st.integers(10, 1000))
+    @settings(max_examples=10, deadline=None)
+    def test_each_batch_has_correct_size_and_shape(self, n_channels, number_of_paths):
+        '''
+        Check that the returned image has the correct size and shape
+        Given:
+            - numebr of channels
+            - number of paths
+        Then:
+            - create the test img and target path list
+            - init the ImageFeederOnTheFly
+            - get random item
+        Assert:
+            - len of the item is 2
+            - shape of the first element is  (n_channels, 64, 64)
+            - shape of the seconf element is (1, 64, 64)
+        '''
+
+        img_paths = number_of_paths * ['test/test_images/test_image.nii']
+        imgs = n_channels * [img_paths]
+        tar_paths = number_of_paths * ['test/test_images/test_target.nii'] 
+        imfeeder = feeder.ImageFeederOnTheFly(*imgs, tar_paths,)
+        # get the reference element idex
+        idx = np.random.randint(0, len(imfeeder))
+        item = imfeeder[idx]
+
+        assert len(item) == 2
+        assert item[0].shape == (n_channels, 64, 64)
+        assert item[1].shape == (1, 64, 64)
+
+
